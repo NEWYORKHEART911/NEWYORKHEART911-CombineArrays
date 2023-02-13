ae2dbc112a10d22f877953b04110e06833e7587f
NEWYORKHEART911 <lavillelumiere8@gmail.com>
2023-02-13 20:07:12 +0000

    changes1

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,15 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int measure = arr1.length + arr2.length;
+        int[] arr3 = new int[measure];
+        for(int j=0; j<measure; j++) {
+            if(j<arr1.length) {
+                arr3[j]=arr1[j];
+            } else {
+                arr3[j]=arr2[j-arr1.length];
+            }
+        }
+        return arr3;
     }
 }
